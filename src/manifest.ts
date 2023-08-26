e9a0143c8e8f43b98578dc141434282f0a34ce0e
Hocky Yudhiono <hocky.yudhiono@tiket.com>
2023-08-27 05:13:37 +0700

    Change manifest to use proper logo

--- src/manifest.ts
+++ src/manifest.ts
@@ -6,14 +6,14 @@ export default defineManifest({
   version: '1.0.0',
   manifest_version: 3,
   icons: {
-    '16': 'img/logo-16.png',
-    '32': 'img/logo-34.png',
-    '48': 'img/logo-48.png',
-    '128': 'img/logo-128.png',
+    '16': 'img/icon16.png',
+    '32': 'img/icon32.png',
+    '48': 'img/icon48.png',
+    '128': 'img/icon128.png',
   },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/icon48.png',
   },
   options_page: 'options.html',
   background: {
@@ -28,7 +28,7 @@ export default defineManifest({
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: ['img/icon16.png', 'img/icon32.png', 'img/icon48.png', 'img/icon128.png'],
       matches: [],
     },
   ],
